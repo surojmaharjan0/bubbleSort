76541c54cacb87c9a060e602224ff86c248bfd2b
Suroj Maharjan <38045304+surojmaharjan0@users.noreply.github.com>
2018-04-07 21:48:13 +0545

    completed the code

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,9 +5,9 @@ public class BubbleSort{
         //calling function to sort (in ascending order)
         bubbleSort(a);
         //print sorted array
-        System.out.println("Sorted array:")
+        System.out.println("Sorted array:");
         for(int i:a){
-            System.out.printf("%d",i);
+            System.out.printf("%d\t",i);
         }
     }
     private static void bubbleSort(int[]a){
@@ -15,5 +15,32 @@ public class BubbleSort{
         i.e after 1 iteration, the larges element bubbles up 
         at end of array
         */
+        int sortedIndex=a.length-1;
+        while(sortedIndex>0){
+            for(int i=0;i<sortedIndex;i++){
+                if(a[i]>a[i+1]){
+                    //if currently scanned number is larger, swap
+                    int t=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=t;
+                }
+            }
+            sortedIndex--; //one more number gets sorted
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
